65452938da5d7d1e471766fe87af1ae207097a6c
Boyan Valchev <valchev.boian@gmail.com>
2021-05-30 23:53:23 +0300

    Fixing chat component

--- fundjoy/src/components/Chat/ChatComponent.js
+++ fundjoy/src/components/Chat/ChatComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Launcher} from 'react-chat-window'
 import io from 'socket.io-client'
 const socket = io.connect('http://localhost:5000', {
@@ -7,78 +7,75 @@ const socket = io.connect('http://localhost:5000', {
 });
 
  
-class ChatComponent extends Component {
+class ChatComponent extends React.Component {
+  socket;
   constructor() {
     super();
+    this.socket = socket;
+    this.handleNewMessageSocketResponse = this.handleNewMessageSocketResponse.bind(this);
+    this.appendMessage = this.appendMessage.bind(this);
+
     this.state = {
-      messageList: [],
+      messageList: []
     };
+    
   }
 
-  componentDidMount() {
-    socket.on("new message", function (response) {
-      let username = JSON.parse(localStorage.getItem("currentUser")).user
-        .username;
+  
+  componentDidMount(){
+    this.handleNewMessageSocketResponse();
+  }
+
+  handleNewMessageSocketResponse() {
+    this.socket.on( 'new message', (response) => this.appendMessage(response))
+      
+  }
+
+  appendMessage(response) {
+      let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
       let message = response.msg;
-      if (response.author !== username) {
+      console.log(response)
+      if(response.author !== username){
+        console.log(((this.state || {}).messageList || []), {
+          author: response.author,
+          type: 'text',
+          data: { message }
+        })
         this.setState({
-          messageList: [
-            ...(this.state ? this.state.messageList : []),
-            {
-              author: response.author,
-              type: "text",
-              data: { message },
-            },
-          ]
-        });
+          messageList: [...((this.state || {}).messageList || []), {
+            author: response.author,
+            type: 'text',
+            data: {...message }
+          }]
+        })
       }
-    });
   }
 
   _onMessageWasSent(message) {
-    let username = JSON.parse(localStorage.getItem("currentUser")).user
-      .username;
+    let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
     this.setState({
-      messageList: [...(this.state ? this.state.messageList : []), message],
-    });
-    socket.emit("send message", {
+      messageList: [...((this.state || {}).messageList || []), message]
+    })
+    this.socket.emit('send message', {
       author: username,
-      type: "text",
-      data: { message },
-    });
+      type: 'text',
+      data: { message
+      }
+    })
   }
-
-  // _sendMessage(text) {
-  //   let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
-
-  //   socket.on( 'new message', function (response) {
-  //     let username = JSON.parse(localStorage.getItem('currentUser')).user.username;
-  //     let message = response.msg;
-  //     if(response.author !== username)
-  //     this.setState({
-  //       messageList: [...this.state.messageList, {
-  //         author: response.author,
-  //         type: 'text',
-  //         data: { message }
-  //       }]
-  //     })
-  //   })
-  // }
-
+ 
   render() {
-    return (
-      <div>
-        <Launcher
-          agentProfile={{
-            teamName: "Support",
-            imageUrl: "../Chat/ChatImage.png",
-          }}
-          onMessageWasSent={this._onMessageWasSent.bind(this)}
-          messageList={this.state.messageList}
-          showEmoji
-        />
-      </div>
-    );
+    return (<div>
+      <Launcher
+        agentProfile={{
+          teamName: 'Support',
+          imageUrl: '../Chat/ChatImage.png'
+        }}
+        onMessageWasSent={this._onMessageWasSent.bind(this)}
+        messageList={((this.state || {}).messageList || [])}
+        showEmoji
+      />
+    </div>)
   }
 }
 
